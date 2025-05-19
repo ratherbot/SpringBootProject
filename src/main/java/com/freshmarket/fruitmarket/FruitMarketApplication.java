5dc36f3e493ec4db2b675f913a75a2b3b2dbc2b9
ratherbot <just51290@mail.ru>
2025-05-19 23:04:10 +0300

    init

---
+++ src/main/java/com/freshmarket/fruitmarket/FruitMarketApplication.java
@@ -0,0 +1,13 @@
+package com.freshmarket.fruitmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freshmarket/fruitmarket/FruitMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshmarket.fruitmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
